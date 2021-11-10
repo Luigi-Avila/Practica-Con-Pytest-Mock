630a6a9a68ff8acdb108864013e069b1a0c4b863
Luigi-Avila <l.avila.uvg@gmail.com>
2021-11-10 12:38:35 -0600

    correccion y descripcion de los casos de uso (Excel)

--- test/test_main_study.py
+++ test/test_main_study.py
@@ -1,10 +1,11 @@
 from unittest import mock
+from py import process
 import pytest
 from monitor import main
 
-#Prueba de integracion para la funcion de calculation_suma
+#Prueba unitaria para la funcion de calculation_suma
 #Particion de equivalencia
-# numeros de entrada y salida positivos, negativos y ceros
+# numeros de entrada y salida positivos, negativos ceros y decimales
 numerosArraySumaExpected = [
     (2, -1, 1),
     (-1, 0, -1),
@@ -18,13 +19,15 @@ def test_calculation_suma(numberA, numberB, expected):
     process = main.calculation_suma(numberA, numberB)
     assert process == expected_result
 
-#Prueba de integracion para la funcion de calculation_resta
+#Prueba unitaria para la funcion de calculation_resta
 #Particion de equivalencia
-#Numeros de entrada y salida positivos, negativos y ceros
+#Numeros de entrada y salida positivos, negativos, ceros y decimales
 numerosArrayRestaExpected = [
     (1, -1, 2),
     (-1, 0, -1),
     (0, 0, 0),
+    (0.8, -0.1, 0.9),
+    (-0.8, 0.1, -0.9)
 ]
 @pytest.mark.parametrize("numberA, numberB, expected", numerosArrayRestaExpected)
 def test_calculation_resta(numberA, numberB, expected):
@@ -32,21 +35,66 @@ def test_calculation_resta(numberA, numberB, expected):
     process = main.calculation_resta(numberA, numberB)
     assert process == expected_result
 
-#Prueba de integracion para la funcion de calculation_multiplication
+#Prueba unitaria para la funcion de calculation_multiplication
 #Particion de equivalencia
-#Numeros de entrada y salida positivos, negativos y ceros
+#Numeros de entrada y salida positivos, negativos, ceros y decimales
 numerosArrayMultiExpected = [
     (1, -1, -1),
     (-2, 0, 0),
-    (0, 1, 0),
     (1, 1, 1),
-    (0.9, -1, -0.9)
+    (0.9, -0.1, -0.09)
 ]
 @pytest.mark.parametrize("numberA, numberB, expected", numerosArrayMultiExpected)
 def test_calculation_multiplicacion(numberA, numberB, expected):
     expected_result = expected
     process = main.calculation_multiplicacion(numberA, numberB)
     assert process == expected_result
+
+# Pruebas de integracion para la funcion de valida
+
+numerosArrayValidaExpected = [
+    (1, 1, 1, 'success'),
+    (' ', ' ', ' ', 'Fail'),
+    (0, 0, 0, 'success')
+]
+@pytest.mark.parametrize("numberA, numberB, value, expected", numerosArrayValidaExpected)
+def test_valida(numberA, numberB, value, expected):
+    with mock.patch('monitor.main.calculation_suma', return_value = value):
+        with mock.patch('monitor.main.calculation_multiplicacion', return_value = value):
+            process = main.valida(numberA, numberB)
+    expected_result = expected
+    assert process == expected_result
+
+# Prueba de integracion para la funcion positive_negative
+# Particion de equivalencia y analisis de valores limite
+numerosArraySumPos_negativeExpected = [
+    (0, 0.1, 0.1, 'positive'),
+    (0, 0, 0, 'neutro'),
+    (-0.1, 0, -0.1, 'negative')
+]
+@pytest.mark.parametrize("numberA, numberB, value, expected", numerosArraySumPos_negativeExpected)
+def test_positive_negative(numberA, numberB, value, expected):
+    with mock.patch('monitor.main.calculation_suma', return_value = value):
+        process = main.positive_negative(numberA, numberB)
+    expected_result = expected
+    assert process == expected_result
+
+
+# MIS PRACTICAS
+# #Prueba de integracion para la funcion de valida
+# # Enfoque al resultado
+# numerosArrayValidaExpected = [
+#     (1, -0.1, 'success'),
+#     (0, 0.1, 'success'),
+#     (0, 0, 'success'),
+#     ( 'b', 'a', 'Fail')
+# ]
+
+# @pytest.mark.parametrize("numberA, numberB, expected", numerosArrayValidaExpected)
+# def test_valida(numberA, numberB, expected):
+#     expected_result = expected
+#     process_Suma = main.valida(numberA, numberB)
+#     assert process_Suma == expected_result
 
 #Prueba de integracion para la funcion de positive_negative
 #mock y parametrize
@@ -71,4 +119,4 @@ def test_calculation_multiplicacion(numberA, numberB, expected):
 #         process = main.positive_negative(numberA, numberB)
 #         expected_result = expectedStr
 #     assert process == expected_result
-    
+
